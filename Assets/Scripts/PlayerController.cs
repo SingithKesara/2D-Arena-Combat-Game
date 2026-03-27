deed8d790733fe348d034ca171be2c8d991da826
Kesh <singithkesarawahalathanthri@gmail.com>
2026-03-27 11:40:10 +0530

    basic animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,41 @@ public class PlayerController : MonoBehaviour
     public float walkSpeed = 5f;
     Vector2 moveInput;
 
-    public bool IsMoving { get; private set; }
+    private bool _isMoving = false;
+
+    public bool IsMoving { get
+        {
+            return _isMoving;
+        }
+        private set 
+        { 
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+    private bool _isRunning = false;
+
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
